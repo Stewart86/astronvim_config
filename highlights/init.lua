ec1518fa5a18587f5e0ba2377f7cdfefb62b07e2
Stewart Wong <siwei.wong@gmail.com>
2023-03-15 00:44:36 +0800

    fix unused var

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,10 +1,9 @@
 return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
-  local normal = get_hlgroup "Normal"
   local cursor_nr = get_hlgroup "GruvboxOrangeSign"
   local line_nr = get_hlgroup "GruvboxBg4"
   local winbar = get_hlgroup "Winbar"
-  local fg, bg = normal.fg, "NONE"
+  local bg = "NONE"
   return {
     Winbar = { fg = winbar.fg, bg = bg },
     FoldColumn = { fg = line_nr.fg, bg = bg },
