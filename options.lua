9a6dbb8598f2e9202312dfaf95dab184b10d680b
Stewart Wong <siwei.wong@gmail.com>
2023-04-03 10:50:37 +0800

    minor update and add readme

--- options.lua
+++ options.lua
@@ -1,44 +1,38 @@
--- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true,         -- sets vim.opt.number
-    spell = false,         -- sets vim.opt.spell
-    signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
-    wrap = false,          -- sets vim.opt.wrap
+    relativenumber = true,
+    number = true,
+    spell = false,
+    signcolumn = "auto",
+    wrap = true,
+    showbreak = "↪ ",
     foldcolumn = "1",
     background = "dark",
+    swapfile = false,
   },
   g = {
-    mapleader = " ",                   -- sets vim.g.mapleader
-    autoformat_enabled = true,         -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true,                -- enable completion at start
-    autopairs_enabled = true,          -- enable autopairs at start
-    diagnostics_enabled = true,        -- enable diagnostics at start
-    status_diagnostics_enabled = true, -- enable diagnostics in statusline
-    icons_enabled = true,              -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = true,   -- disable notifications when toggling UI elements
-    heirline_bufferline = true,        -- enable new heirline based bufferline (requires :PackerSync after changing)
-    loaded_ruby_provider = 0,          -- disable ruby provider
-    loaded_perl_provider = 0,          -- disable perl provider
+    mapleader = " ",
+    autoformat_enabled = true,
+    cmp_enabled = true,
+    autopairs_enabled = true,
+    diagnostics_enabled = true,
+    status_diagnostics_enabled = true,
+    icons_enabled = true,
+    ui_notifications_enabled = true,
+    heirline_bufferline = true,
+    loaded_ruby_provider = 0,
+    loaded_perl_provider = 0,
     clipboard = {
       name = "WslClipboard",
-      copy = { ["+"] = "clip.exe",["*"] = "clip.exe" },
-      paste = {
-        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+      copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
       },
-      cache_enabled = 0,
+      paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+      },
+      cache_enabled = true,
     },
   },
 }
--- If you need more control, you can use the function()...end notation
--- options = function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end,
--- plugins/init.lua
+++ plugins/init.lua
@@ -19,7 +19,7 @@ return {
     lazy = true,
     cmd = "CellularAutomaton",
     keys = {
-      { "<leader>gof",  "<cmd>CellularAutomaton game_of_life<CR>", desc = "game of life" },
+      { "<leader>gof", "<cmd>CellularAutomaton game_of_life<CR>", desc = "game of life" },
       { "<leader>rain", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "make it rain" },
     },
   },
@@ -35,46 +35,29 @@ return {
     config = true,
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      {
-        "andymass/vim-matchup",
-        init = function() vim.g.matchup_matchparen_deferred = 1 end,
-      },
-      {
-        "HiPhish/nvim-ts-rainbow2",
-        config = function()
-          vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
-            callback = function()
-              vim.cmd.TSDisable "rainbow"
-              vim.cmd.TSEnable "rainbow"
-            end,
-          })
-        end,
-      },
-    },
-    opts = {
-      auto_install = true,
-      highlight = { disable = { "help" } },
-      matchup = { enable = true },
-      rainbow = { enable = true },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-  },
-  {
     "ray-x/lsp_signature.nvim",
     config = true,
     event = "InsertEnter",
   },
-
-  -- {
-  --   "HallerPatrick/py_lsp.nvim",
-  --   event = "BufEnter",
-  --   config = true,
-  -- },
-  -- TODO:
-  -- {"mg979/vim-visual-multi"},
+  {
+    "f-person/git-blame.nvim",
+    event = "BufEnter",
+  },
+  {
+    "zbirenbaum/neodim",
+    event = "LspAttach",
+    opts = {
+      alpha = 0.75,
+      blend_color = "#000000",
+      update_in_insert = {
+        enable = true,
+        delay = 100,
+      },
+      hide = {
+        virtual_text = true,
+        signs = true,
+        underline = true,
+      },
+    },
+  },
 }
